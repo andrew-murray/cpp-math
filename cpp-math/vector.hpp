e86affd27cb085481a640f88c27ff80e2de56d78
andrew-murray <andrew.2013.murray@gmail.com>
2014-08-24 02:03:04 +0100

    Add arithmetic, comparison and logical operators to vector classes. Added tests for arithmetic. Needs tests for logical & comparison.

--- cpp-math/vector.hpp
+++ cpp-math/vector.hpp
@@ -39,15 +39,60 @@ __m128 CPPMATH_INLINE swizzle(const __m128& a){
 
 
 
-class float1 {
+class float1{
 public:
-	CPPMATH_INLINE explicit float1(const float& v)
-	{
+	typedef float1 component_type;
+	typedef float value_type;
+
+	class float1comparator{
+	public:
+		CPPMATH_INLINE explicit float1comparator(const __m128& v){
+			_data.reg = v;
+		}
+
+		CPPMATH_INLINE operator __m128 () const {
+			return _data.reg;
+		}
+		
+		CPPMATH_INLINE float1comparator operator == (const float1comparator& other) const {
+			return float1comparator(_mm_cmpeq_ss(_data.reg,(__m128)other));
+		}
+		
+		CPPMATH_INLINE float1comparator operator != (const float1comparator& other) const {
+			return float1comparator(_mm_cmpneq_ss(_data.reg,(__m128)other));
+		}
+		
+		CPPMATH_INLINE float1comparator operator > (const float1comparator& other) const {
+			return float1comparator(_mm_cmpgt_ss(_data.reg,(__m128)other));
+		}
+		
+		CPPMATH_INLINE float1comparator operator >= (const float1comparator& other) const {
+			return float1comparator(_mm_cmpge_ss(_data.reg,(__m128)other));
+		}
+		
+		CPPMATH_INLINE float1comparator operator < (const float1comparator& other) const {
+			return float1comparator(_mm_cmplt_ss(_data.reg,(__m128)other));
+		}
+		
+		CPPMATH_INLINE float1comparator operator <= (const float1comparator& other) const {
+			return float1comparator(_mm_cmple_ss(_data.reg,(__m128)other));
+		}
+		
+		static CPPMATH_INLINE uint16_t mask(const float1comparator& input){
+			__m128i input_as_intvec = _mm_castps_si128((__m128)input);
+			uint16_t returned_mask = _mm_movemask_epi8(input_as_intvec);
+			return returned_mask;
+		}
+
+	private:
+		__f128 _data;
+	};
+
+	CPPMATH_INLINE explicit float1(const float& v){
 		_data.reg = _mm_set_ss(v);
 	}
 
-	CPPMATH_INLINE explicit float1(const __m128& v)
-	{
+	CPPMATH_INLINE explicit float1(const __m128& v){
 		_data.reg = v;
 	}
 
@@ -59,26 +104,104 @@ public:
 		return _data.reg;
 	}
 	
-	CPPMATH_INLINE bool operator != (const float1& other){
-		__m128i vcmp = _mm_castps_si128(_mm_cmpneq_ps(_data.reg,(__m128)other));
-		int test = _mm_movemask_epi8(vcmp);
+	CPPMATH_INLINE bool operator != (const float1& other) const {
+		const float1comparator us((__m128)*this);
+		const float1comparator them((__m128)other);
+		int test = float1comparator::mask(us != them);
 		return (test & 0xF) != 0;
 	}
 	
-	CPPMATH_INLINE bool operator == (const float1& other){
-		__m128i vcmp = _mm_castps_si128(_mm_cmpeq_ps(_data.reg,(__m128)other));
-		int test = _mm_movemask_epi8(vcmp);
-		return (test & 0x000F) != 0;
+	CPPMATH_INLINE bool operator == (const float1& other) const {
+		const float1comparator us((__m128)*this);
+		const float1comparator them((__m128)other);
+		int test = float1comparator::mask(us == them);
+		return (test & 0xF) != 0;
+	}
+	
+	CPPMATH_INLINE float1 operator & (const float1& other) const {
+		return float1(_mm_and_ps(_data.reg,(__m128)other));
+	}
+		
+	CPPMATH_INLINE float1 operator | (const float1& other) const {
+		return float1(_mm_or_ps(_data.reg,(__m128)other));
+	}
+		
+	CPPMATH_INLINE float1 operator ^ (const float1& other) const {
+		return float1(_mm_xor_ps(_data.reg,(__m128)other));
+	}
+
+	
+	CPPMATH_INLINE float1 operator + (const float1& other){
+		return float1(_mm_add_ss(_data.reg,(__m128)other));
+	}
+	
+	CPPMATH_INLINE float1 operator - (const float1& other){
+		return float1(_mm_sub_ss(_data.reg,(__m128)other));
+	}
+	
+	CPPMATH_INLINE float1 operator * (const float1& other){
+		return float1(_mm_mul_ss(_data.reg,(__m128)other));
+	}
+	
+	CPPMATH_INLINE float1 operator / (const float1& other){
+		return float1(_mm_div_ss(_data.reg,(__m128)other));
 	}
 
 private:
 	__f128 _data;
 };
 
-class float4 {
+class float4{
 public:
-	CPPMATH_INLINE float4(const float& x, const float& y, const float& z, const float& w = 0.0f)
-	{
+	typedef float1 component_type;
+	typedef float value_type;
+
+	class float4comparator{
+	public:
+		CPPMATH_INLINE explicit float4comparator(const __m128& v){
+			_data.reg = v;
+		}
+
+		CPPMATH_INLINE operator __m128 () const {
+			return _data.reg;
+		}
+		
+		CPPMATH_INLINE float4comparator operator == (const float4comparator& other) const {
+			return float4comparator(_mm_cmpeq_ps(_data.reg,(__m128)other));
+		}
+		
+		CPPMATH_INLINE float4comparator operator != (const float4comparator& other) const {
+			return float4comparator(_mm_cmpneq_ps(_data.reg,(__m128)other));
+		}
+		
+		CPPMATH_INLINE float4comparator operator > (const float4comparator& other) const {
+			return float4comparator(_mm_cmpgt_ps(_data.reg,(__m128)other));
+		}
+		
+		CPPMATH_INLINE float4comparator operator >= (const float4comparator& other) const {
+			return float4comparator(_mm_cmpge_ps(_data.reg,(__m128)other));
+		}
+		
+		CPPMATH_INLINE float4comparator operator < (const float4comparator& other) const {
+			return float4comparator(_mm_cmplt_ps(_data.reg,(__m128)other));
+		}
+		
+		CPPMATH_INLINE float4comparator operator <= (const float4comparator& other) const {
+			return float4comparator(_mm_cmple_ps(_data.reg,(__m128)other));
+		}
+		
+		static CPPMATH_INLINE uint16_t mask(const float4comparator& input){
+			__m128i input_as_intvec = _mm_castps_si128((__m128)input);
+			uint16_t returned_mask = _mm_movemask_epi8(input_as_intvec);
+			return returned_mask;
+		}
+
+	private:
+		__f128 _data;
+	};
+
+
+	CPPMATH_INLINE float4(const float& x, const float& y, const float& z, const float& w = 0.0f){
 		_data.reg = _mm_set_ps(x,y,z,w);
 	}
 
@@ -90,7 +213,6 @@ public:
 		return _data.reg;
 	}
 
-	// these accessors should very definitely use swizzles
 	CPPMATH_INLINE float1 x() const {
 		return float1(swizzle<3,3,3,3>(_data.reg,_data.reg));
 	}
@@ -108,19 +230,47 @@ public:
 	}
 
 	CPPMATH_INLINE bool operator != (const float4& other){
-		__m128i vcmp = _mm_castps_si128(_mm_cmpneq_ps(_data.reg,(__m128)other));
-		uint16_t test = _mm_movemask_epi8(vcmp);
+		const float4comparator us((__m128)*this);
+		const float4comparator them((__m128)other);
+		const uint16_t test = float4comparator::mask(us!=them);
 		return test != 0x0;
 	}
 	
 	CPPMATH_INLINE bool operator == (const float4& other){
-		__m128i vcmp = _mm_castps_si128(_mm_cmpeq_ps(_data.reg,(__m128)other));
-		uint16_t test = _mm_movemask_epi8(vcmp);
+		const float4comparator us((__m128)*this);
+		const float4comparator them((__m128)other);
+		const uint16_t test = float4comparator::mask(us==them);
 		return test == std::numeric_limits<uint16_t>::max();
 	}
 
-private:
+	CPPMATH_INLINE float4 operator & (const float4& other){
+		return float4(_mm_and_ps(_data.reg,(__m128)other));
+	}
+		
+	CPPMATH_INLINE float4 operator | (const float4& other){
+		return float4(_mm_or_ps(_data.reg,(__m128)other));
+	}
+		
+	CPPMATH_INLINE float4 operator ^ (const float4& other){
+		return float4(_mm_xor_ps(_data.reg,(__m128)other));
+	}
 
+	CPPMATH_INLINE float4 operator + (const float4& other){
+		return float4(_mm_add_ps(_data.reg,(__m128)other));
+	}
+	
+	CPPMATH_INLINE float4 operator - (const float4& other){
+		return float4(_mm_sub_ps(_data.reg,(__m128)other));
+	}
+	
+	CPPMATH_INLINE float4 operator * (const float4& other){
+		return float4(_mm_mul_ps(_data.reg,(__m128)other));
+	}
+	
+	CPPMATH_INLINE float4 operator / (const float4& other){
+		return float4(_mm_div_ps(_data.reg,(__m128)other));
+	}
+
+private:
 	__f128 _data;
 };
-
--- test/vector_test.cpp
+++ test/vector_test.cpp
@@ -2,7 +2,7 @@
 
 #include "cpp-math/vector.hpp"
 
-BOOST_AUTO_TEST_CASE(construction){
+BOOST_AUTO_TEST_CASE(creation){
 
 	{
 		const float v = 99.12e5f;
@@ -37,12 +37,11 @@ BOOST_AUTO_TEST_CASE(construction){
 		const float1 c(5.7f);
 		const float1 d(std::numeric_limits<float>::quiet_NaN());
 
-
 		BOOST_CHECK_EQUAL(a, c);
 		BOOST_CHECK_NE(a, b);
+
 		float1 x = b;
 		BOOST_CHECK_EQUAL(x, b);
-		
 		BOOST_CHECK_NE(d, c);
 		
 		BOOST_CHECK_EQUAL(a, a);
@@ -75,31 +74,184 @@ BOOST_AUTO_TEST_CASE(construction){
 
 	}
 
-	{
-		
-		float x = 6.7f;
-		float y = 7.8f;
-		float z = -5.2f;
-		float w = -0.0f;
-		
-		float4 xyzw(x, y, z, w);
-		float4 wzyx(swizzle<0,1,2,3>(xyzw));
-		float4 xwzy(swizzle<3,0,1,2>(xyzw));
-		
-		BOOST_CHECK_EQUAL((float)xyzw.x(),x);
-		BOOST_CHECK_EQUAL((float)xyzw.y(),y);
-		BOOST_CHECK_EQUAL((float)xyzw.z(),z);
-		BOOST_CHECK_EQUAL((float)xyzw.w(),w);
+}
 
-		BOOST_CHECK_EQUAL((float)wzyx.x(),w);
-		BOOST_CHECK_EQUAL((float)wzyx.y(),z);
-		BOOST_CHECK_EQUAL((float)wzyx.z(),y);
-		BOOST_CHECK_EQUAL((float)wzyx.w(),x);
+BOOST_AUTO_TEST_CASE(swizzling){
+
+	float x = 6.7f;
+	float y = 7.8f;
+	float z = -5.2f;
+	float w = -0.0f;
 		
-		BOOST_CHECK_EQUAL((float)xwzy.x(),x);
-		BOOST_CHECK_EQUAL((float)xwzy.y(),w);
-		BOOST_CHECK_EQUAL((float)xwzy.z(),z);
-		BOOST_CHECK_EQUAL((float)xwzy.w(),y);
+	float4 xyzw(x, y, z, w);
+	float4 wzyx(swizzle<0,1,2,3>(xyzw));
+	float4 xwzy(swizzle<3,0,1,2>(xyzw));
+		
+	BOOST_CHECK_EQUAL((float)xyzw.x(),x);
+	BOOST_CHECK_EQUAL((float)xyzw.y(),y);
+	BOOST_CHECK_EQUAL((float)xyzw.z(),z);
+	BOOST_CHECK_EQUAL((float)xyzw.w(),w);
+
+	BOOST_CHECK_EQUAL((float)wzyx.x(),w);
+	BOOST_CHECK_EQUAL((float)wzyx.y(),z);
+	BOOST_CHECK_EQUAL((float)wzyx.z(),y);
+	BOOST_CHECK_EQUAL((float)wzyx.w(),x);
+		
+	BOOST_CHECK_EQUAL((float)xwzy.x(),x);
+	BOOST_CHECK_EQUAL((float)xwzy.y(),w);
+	BOOST_CHECK_EQUAL((float)xwzy.z(),z);
+	BOOST_CHECK_EQUAL((float)xwzy.w(),y);
+
+}
+
+BOOST_AUTO_TEST_CASE(comparator_tests){
+
+	// TODO :
+
+}
+
+BOOST_AUTO_TEST_CASE(float1_arithmetic_tests){
+
+	// addition
+	{
+		float1 a(7.7f);
+		float1 b(-7.7f);
+		BOOST_CHECK_EQUAL(a+b, float1(0.0f));
+		float1 c(114.3f);
+		BOOST_CHECK_EQUAL(a+c,float1((float)a + (float)c));
+		BOOST_CHECK_EQUAL((float)a+c,(float)a + (float)c);
+	}
+
+	// subtraction
+	{
+		float1 a(3.3f);
+		float1 b(8.8f);
+		float1 c(-5.5f);
+		float1 d(5.5f);
+		
+		BOOST_CHECK_EQUAL((float)(a+b),3.3f + 8.8f);
+		BOOST_CHECK_EQUAL((float)(a-b),3.3f - 8.8f);
+		BOOST_CHECK_EQUAL(a + c, a - d);
+		BOOST_CHECK_EQUAL(a + d, a - c);
+		BOOST_CHECK_EQUAL((float)(c + d), 0.0f);
+	}
+
+	// multiplication
+	{
+		float1 a(3.1f);
+		float1 b(8.8f);
+		float1 c(-2.5f);
+		float1 d(2.5f);
+		
+		BOOST_CHECK_EQUAL(a*b, ((float)a) * ((float)b));
+		BOOST_CHECK_EQUAL(b*c, ((float)b) * ((float)c));
+		BOOST_CHECK_EQUAL(b*c, ((float)b) * ((float)c));
+		BOOST_CHECK_EQUAL(c*d, ((float)c) * ((float)d));
+	}
+
+	// division
+	{
+		float1 a(3.1f);
+		float1 b(8.8f);
+		float1 c(-2.5f);
+		float1 d(2.5f);
+		
+		BOOST_CHECK_EQUAL(a/b, ((float)a) / ((float)b));
+		BOOST_CHECK_EQUAL(b/c, ((float)b) / ((float)c));
+		BOOST_CHECK_EQUAL(b/c, ((float)b) / ((float)c));
+		BOOST_CHECK_EQUAL(c/d, ((float)c) / ((float)d));
+	}
+
+}
+
+BOOST_AUTO_TEST_CASE(float4_arithmetic_tests){
+
+	// addition
+	{
+		float x[] = {3.2f, 6.6f, 1.9f};
+		float y[] = {-5.2f, 9.4f, 6.7f};
+		float z[] = {3.0f, 4.7f, 2.8f};
+		float w[] = {7.5f, 0.5f,-4.3f};
+
+		float4 a(x[0], y[0], z[0], w[0]);
+		float4 b(x[1], y[1], z[1], w[1]);
+		float4 c(x[2], y[2], z[2], w[2]);
+		
+		BOOST_CHECK_EQUAL((a+b).x(),float1(x[0] + x[1]));
+		BOOST_CHECK_EQUAL((a+b).y(),float1(y[0] + y[1]));
+		BOOST_CHECK_EQUAL((a+b).z(),float1(z[0] + z[1]));
+		BOOST_CHECK_EQUAL((a+b).w(),float1(w[0] + w[1]));
+
+		BOOST_CHECK_EQUAL((b+c).x(),float1(x[1] + x[2]));
+		BOOST_CHECK_EQUAL((b+c).y(),float1(y[1] + y[2]));
+		BOOST_CHECK_EQUAL((b+c).z(),float1(z[1] + z[2]));
+		BOOST_CHECK_EQUAL((b+c).w(),float1(w[1] + w[2]));
+	}
+
+	// subtraction
+	{
+		float x[] = {3.2f, 6.6f, 1.9f};
+		float y[] = {-5.2f, 9.4f, 6.7f};
+		float z[] = {3.0f, 4.7f, 2.8f};
+		float w[] = {7.5f, 0.5f,-4.3f};
+
+		float4 a(x[0], y[0], z[0], w[0]);
+		float4 b(x[1], y[1], z[1], w[1]);
+		float4 c(x[2], y[2], z[2], w[2]);
+		
+		BOOST_CHECK_EQUAL((a-b).x(),float1(x[0] - x[1]));
+		BOOST_CHECK_EQUAL((a-b).y(),float1(y[0] - y[1]));
+		BOOST_CHECK_EQUAL((a-b).z(),float1(z[0] - z[1]));
+		BOOST_CHECK_EQUAL((a-b).w(),float1(w[0] - w[1]));
+
+		BOOST_CHECK_EQUAL((b-c).x(),float1(x[1] - x[2]));
+		BOOST_CHECK_EQUAL((b-c).y(),float1(y[1] - y[2]));
+		BOOST_CHECK_EQUAL((b-c).z(),float1(z[1] - z[2]));
+		BOOST_CHECK_EQUAL((b-c).w(),float1(w[1] - w[2]));
+	}
+
+	// multiplication
+	{
+		float x[] = {3.2f, 6.6f, 1.9f};
+		float y[] = {-5.2f, 9.4f, 6.7f};
+		float z[] = {3.0f, 4.7f, 2.8f};
+		float w[] = {7.5f, 0.5f,-4.3f};
+
+		float4 a(x[0], y[0], z[0], w[0]);
+		float4 b(x[1], y[1], z[1], w[1]);
+		float4 c(x[2], y[2], z[2], w[2]);
+		
+		BOOST_CHECK_EQUAL((a*b).x(),float1(x[0] * x[1]));
+		BOOST_CHECK_EQUAL((a*b).y(),float1(y[0] * y[1]));
+		BOOST_CHECK_EQUAL((a*b).z(),float1(z[0] * z[1]));
+		BOOST_CHECK_EQUAL((a*b).w(),float1(w[0] * w[1]));
+
+		BOOST_CHECK_EQUAL((b*c).x(),float1(x[1] * x[2]));
+		BOOST_CHECK_EQUAL((b*c).y(),float1(y[1] * y[2]));
+		BOOST_CHECK_EQUAL((b*c).z(),float1(z[1] * z[2]));
+		BOOST_CHECK_EQUAL((b*c).w(),float1(w[1] * w[2]));
+	}
+
+	// division
+	{
+		float x[] = {3.2f, 6.6f, 1.9f};
+		float y[] = {-5.2f, 9.4f, 6.7f};
+		float z[] = {3.0f, 4.7f, 2.8f};
+		float w[] = {7.5f, 0.5f,-4.3f};
+
+		float4 a(x[0], y[0], z[0], w[0]);
+		float4 b(x[1], y[1], z[1], w[1]);
+		float4 c(x[2], y[2], z[2], w[2]);
+		
+		BOOST_CHECK_EQUAL((a/b).x(),float1(x[0] / x[1]));
+		BOOST_CHECK_EQUAL((a/b).y(),float1(y[0] / y[1]));
+		BOOST_CHECK_EQUAL((a/b).z(),float1(z[0] / z[1]));
+		BOOST_CHECK_EQUAL((a/b).w(),float1(w[0] / w[1]));
+
+		BOOST_CHECK_EQUAL((b/c).x(),float1(x[1] / x[2]));
+		BOOST_CHECK_EQUAL((b/c).y(),float1(y[1] / y[2]));
+		BOOST_CHECK_EQUAL((b/c).z(),float1(z[1] / z[2]));
+		BOOST_CHECK_EQUAL((b/c).w(),float1(w[1] / w[2]));
 	}
 
 }
